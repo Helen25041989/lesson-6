6186bca33a00537d7f2e1c41e6065e4ded721ac5
Sergo <TEW6rfHz0>
2022-09-06 22:12:40 +0300

    Добавьте файлы проекта.

---
+++ Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите числа через запятую");
+string numberStr = Console.ReadLine();
+
+string[] numbers = numberStr.Split(',');
+
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (int.Parse(numbers[i]) > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Количество чисел больше нуля = {0}",count);
---
+++ Task43/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите значение b1");
+int b1= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+int k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+int b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+int k2 = int.Parse(Console.ReadLine());
+
+double x = (double)(b2 - b1) / (k1 - k2);
+double y = (double) k1 * x + b1;
+Console.WriteLine("Точка перечения = ({0};{1})",x,y);
